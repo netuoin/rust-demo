ffc0eceed6f857ecf04288ae250044136b743596
lushfe <admin@lushfe.com>
2024-11-16 13:53:15 +0800

    gx

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,24 @@ fn main() {
         .output()
         .expect("Failed to start v2ray");
 
-        //     Value       Name
-        // 1   59.30KB     inbound>>>api>>>traffic>>>downlink
-        // 2   32.92KB     inbound>>>api>>>traffic>>>uplink
-        // 3   2.95MB      inbound>>>tcp>>>traffic>>>downlink
-        // 4   2.39MB      inbound>>>tcp>>>traffic>>>uplink
-        // 5   2.44MB      outbound>>>direct>>>traffic>>>downlink
-        // 6   2.26MB      outbound>>>direct>>>traffic>>>uplink
-        // 7   2.44MB      user>>>userA>>>traffic>>>downlink
-        // 8   2.26MB      user>>>userA>>>traffic>>>uplink
-        
+    //     Value       Name
+    //     59.30KB     a1
+    //     32.92KB     a2
+    //     2.95MB      a3
+    //     2.39MB      a4
+    //     2.44MB      a5
+    //     2.26MB      a6
+    //     2.44MB      a7
+    //     2.26MB      a8
 
-    // 输出流量数据,转换为json
-    let output = String::from_utf8_lossy(&output.stdout);
-    let output = output.replace(" ", "");
-    let output = output.replace(">>>", "_");
-    let output = output.replace("Value", "\"Value\"");
-    let output = output.replace("Name", "\"Name\"");
-    let output = output.replace("\n", ",");
-    let output = format!("{{{}}}", output);
-    println!("{}", output);
+    // 输出流量数据,转换为json格式
+    // {"name":"a1","value":"59.30KB"}
+    // {"name":"a2","value":"32.92KB"}
+    // {"name":"a3","value":"2.95MB"}
+    // {"name":"a4","value":"2.39MB"}
+    // {"name":"a5","value":"2.44MB"}
+    // {"name":"a6","value":"2.26MB"}
+    // {"name":"a7","value":"2.44MB"}
+    // {"name":"a8","value":"2.26MB"}
+    println!("{}", String::from_utf8_lossy(&output.stdout));
 }
