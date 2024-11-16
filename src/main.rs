aa4a78cea9f527dbb0f9b5c771a27bc15576a038
lushfe <admin@lushfe.com>
2024-11-16 14:01:37 +0800

    gx

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,5 @@ fn main() {
         .arg("stats")
         .output()
         .expect("Failed to start v2ray");
-    // 接收命令行参数
-    let args: Vec<String> = std::env::args().collect();
-    // 赋值给变量
-    let mut traffic: f64 = 0.0;
-    // 判断是否有参数
-    if args.len() > 1 {
-        // 判断参数是否为数字
-        if let Ok(num) = args[1].parse::<f64>() {
-            // 赋值给变量
-            traffic = num;
-        }
-    }
-    // 打印
-    println!("traffic: {}", traffic);
+    print!("{}", String::from_utf8_lossy(&output.stdout));
 }
