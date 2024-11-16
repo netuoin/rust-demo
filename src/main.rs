f54e42927675806c3b4f8968520b65c7fd6d861e
lushfe <admin@lushfe.com>
2024-11-16 13:48:01 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::process::Command;
 
 fn main() {
-    Command::new("v2ray")
+    // 通过v2ray的api获取到流量数据
+    let output = Command::new("v2ray")
         .arg("api")
         .arg("stats")
-        .spawn()
+        .output()
         .expect("Failed to start v2ray");
+
+    // 解析v2ray的api返回的数据
+    let output_str = String::from_utf8(output.stdout).unwrap();
+    println!("{}", output_str);
 }
