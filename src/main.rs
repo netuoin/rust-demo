a01935490424c2a94445aec9f8636a98a2963292
lushfe <admin@lushfe.com>
2024-11-16 13:56:10 +0800

    gx

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,39 @@ fn main() {
         .output()
         .expect("Failed to start v2ray");
 
-    //     Value       Name
-    //     59.30KB     a1
-    //     32.92KB     a2
-    //     2.95MB      a3
-    //     2.39MB      a4
-    //     2.44MB      a5
-    //     2.26MB      a6
-    //     2.44MB      a7
-    //     2.26MB      a8
+    // 控制台输入
+    // Value       Name
+    // 59.30KB     a1
+    // 32.92KB     a2
+    // 2.95MB      a3
+    // 2.39MB      a4
+    // 2.44MB      a5
+    // 2.26MB      a6
+    // 2.44MB      a7
+    // 2.26MB      a8
 
-    // 输出流量数据,转换为json格式
-    // {"name":"a1","value":"59.30KB"}
-    // {"name":"a2","value":"32.92KB"}
-    // {"name":"a3","value":"2.95MB"}
-    // {"name":"a4","value":"2.39MB"}
-    // {"name":"a5","value":"2.44MB"}
-    // {"name":"a6","value":"2.26MB"}
-    // {"name":"a7","value":"2.44MB"}
-    // {"name":"a8","value":"2.26MB"}
-    println!("{}", String::from_utf8_lossy(&output.stdout));
+    // 转换为json
+    // {
+    //     "a1": "59.30KB",
+    //     "a2": "32.92KB",
+    //     "a3": "2.95MB",
+    //     "a4": "2.39MB",
+    //     "a5": "2.44MB",
+    //     "a6": "2.26MB",
+    //     "a7": "2.44MB",
+    //     "a8": "2.26MB"
+    // }
+    let output = String::from_utf8_lossy(&output.stdout);
+    let output = output.split("\n").collect::<Vec<&str>>();
+    let mut json = "{".to_string();
+    for i in 1..output.len() {
+        let line = output[i].split_whitespace().collect::<Vec<&str>>();
+        if line.len() == 2 {
+            json.push_str(&format!("\"{}\": \"{}\",\n", line[1], line[0]));
+        }
+    }
+    json.pop();
+    json.pop();
+    json.push('}');
+    println!("{}", json);
 }
